ca76c312f181d7beb907729e0298690ecad309b6
Caroline Dirat <32294659+CarolineDirat@users.noreply.github.com>
2020-11-13 17:27:52 +0100

    delete tests/Controller/DefaultControllerTest.php, and create tests/PagesTest.php to tests severals URIs

---
+++ tests/PagesTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class PagesTest extends WebTestCase
+{
+    /**
+     * testPageIsSuccessFull
+     * 
+     * @dataProvider provideUrls
+     *
+     * @param  string $url
+     */
+    public function testPageIsSuccessFull(string $url): void
+    {
+        $client = self::createClient();
+        $client->followRedirects();
+        $client->request('GET', $url);
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+    
+    /**
+     * provideUrls
+     *
+     * @return array<array<string>>
+     */
+    public function provideUrls(): array
+    {
+        return [
+            ['/'],
+            ['/login'],
+            ['/tasks'],
+            ['/tasks/create'],
+            ['/users'],
+            ['/users/create'],
+        ];
+    }
+}
