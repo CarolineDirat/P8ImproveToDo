40b102905f36d459fb1fd2c181521202dde29c07
Caroline Dirat <32294659+CarolineDirat@users.noreply.github.com>
2020-11-23 11:27:27 +0100

    edit src/DataFixtures/AppFixtures.php to load 51 tasks (25 done)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Task;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,16 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // 51 tasks, 25 are done
+        for ($i = 1; $i <= 51; ++$i) {
+            $task = new Task();
+            $task->setTitle('Titre de la tâche n°'.$i);
+            $task->setContent('Texte du contenu de la tâche n°'.$i);
+            if (0 === $i % 2) {
+                $task->toggle(true);
+            }
+            $manager->persist($task);
+        }
 
         $manager->flush();
     }
