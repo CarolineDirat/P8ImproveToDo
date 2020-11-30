aa2b9a462b4829dcd403783639bd8a81af231958
Caroline Dirat <32294659+CarolineDirat@users.noreply.github.com>
2020-11-30 14:42:15 +0100

    Behat : edit 1-user-new.feature, 1-users.feature and features/bootstrap/FeatureContext.php

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,15 +1,31 @@
 <?php
 
+use App\Entity\User;
+use App\Kernel;
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Gherkin\Node\TableNode;
+use Behat\Mink\Driver\BrowserKitDriver;
 use Behat\Mink\Driver\Selenium2Driver;
+use Behat\Mink\Exception\UnsupportedDriverActionException;
 use Behat\MinkExtension\Context\MinkContext;
+use Symfony\Component\BrowserKit\Client;
 use WebDriver\Exception\NoAlertOpenError;
+
+require_once __DIR__.'/../../config/bootstrap.php';
+require_once __DIR__.'/../../src/Kernel.php';
 
 /**
  * Defines application features from the specific context.
  */
 class FeatureContext extends MinkContext
 {
+    /**
+     * container.
+     *
+     * @var mixed
+     */
+    private static $container;
+
     /**
      * Initializes context.
      *
@@ -24,7 +40,7 @@ class FeatureContext extends MinkContext
     /**
      * @Given I am authenticated user as :username with :passsword
      */
-    public function iAmAuthenticatedUserAsWith(string $username, string $password)
+    public function iAmAuthenticatedUserAsWith(string $username, string $password): void
     {
         $this->visit('/login');
         $this->fillField('username', $username);
@@ -35,7 +51,7 @@ class FeatureContext extends MinkContext
     /**
      * @Given I am on the home page
      */
-    public function iAmOnTheHomePage()
+    public function iAmOnTheHomePage(): void
     {
         $this->visit('/');
     }
@@ -47,7 +63,7 @@ class FeatureContext extends MinkContext
     {
         $title = $this->getSession()->getPage()->find('css', 'h1');
         if ($h1 !== $title->getText()) {
-            throw new Exception('La page obtenue ne contient pas le titre "'.$h1.'"');
+            throw new PendingException('La page obtenue ne contient pas le titre "'.$h1.'" mais : "'.$title->getText().'".');
         }
     }
 
@@ -83,9 +99,70 @@ class FeatureContext extends MinkContext
 
     /**
      * @When I fill in the following
+     *
+     * @param TableNode<array<string>> $table
      */
     public function iFillInTheFollowing(TableNode $table): void
     {
         $this->fillFields($table);
+    }
+
+    /**
+     * @When I wait for :ms
+     */
+    public function iWaitFor(string $ms): void
+    {
+        $this->getSession()->wait((int) $ms);
+    }
+
+    /**
+     * @When I follow redirect
+     */
+    public function iFollowRedirect(): void
+    {
+        $client = $this->getClient();
+        $client->followRedirects();
+        $client->followMetaRefresh();
+    }
+
+    /**
+     * @Then Delete user :username
+     */
+    public function deleteUser(string $username): void
+    {
+        self::bootstrapSymfony();
+        $entityManager = self::$container->get('doctrine')->getManager();
+        $repository = self::$container->get('doctrine')->getRepository(User::class);
+        /** @var User $user */
+        $user = $repository->findOneBy(['username' => 'user']);
+        $entityManager->remove($user);
+        $entityManager->flush();
+    }
+
+    public static function bootstrapSymfony(): void
+    {
+        $kernel = new Kernel('dev', true);
+        $kernel->boot();
+        self::$container = $kernel->getContainer();
+    }
+
+    /**
+     * Returns current active mink session.
+     *
+     * @throws UnsupportedDriverActionException
+     *
+     * @return Client
+     */
+    protected function getClient()
+    {
+        $driver = $this->getSession()->getDriver();
+
+        if (!$driver instanceof BrowserKitDriver) {
+            $message = 'This step is only supported by the browserkit drivers';
+
+            throw new UnsupportedDriverActionException($message, $driver);
+        }
+
+        return $driver->getClient();
     }
 }
